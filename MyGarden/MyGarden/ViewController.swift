5df2d9dc568215eb20e4f15d7f4a6fe3f544868f
Daniel Burgner <dburgnerjr@yahoo.com>
2017-06-22 22:05:10 -0500

    Timer setup
    Setting up initial timer functionality.

--- MyGarden/MyGarden/ViewController.swift
+++ MyGarden/MyGarden/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateTime() {
+        Label.text =
+            DateFormatter.localizedString(from: NSDate() as Date, dateStyle: DateFormatter.Style.none, timeStyle: DateFormatter.Style.full)
+    }
 
 }
 
